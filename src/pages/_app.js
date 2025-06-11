c5c7b70ec1b20188162cb4c6b5bcb50c16e1df4d
hiren-digilize <hiren.p@digilize.in>
2025-06-11 22:23:33 +0530

    log added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,5 +3,6 @@ import "@/styles/globals.css";
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+console.log('wewewe !!!!');
 
 console.log('121212')
